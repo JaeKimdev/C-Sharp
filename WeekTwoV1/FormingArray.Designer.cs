385360e4b3aad8b3308880351f7aa0aebac935dd
Jae Hyung Kim <smfc.mene@gmail.com>
2022-02-08 10:40:35 +0800

    Week2
    Studying 2D Array

---
+++ WeekTwoV1/FormingArray.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WeekTwoV1
+{
+    partial class FormingArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonRandomize = new System.Windows.Forms.Button();
+            this.ButtonInitialize = new System.Windows.Forms.Button();
+            this.ButtonSort = new System.Windows.Forms.Button();
+            this.ListboxDisplay = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ButtonRandomize
+            // 
+            this.ButtonRandomize.Location = new System.Drawing.Point(21, 12);
+            this.ButtonRandomize.Name = "ButtonRandomize";
+            this.ButtonRandomize.Size = new System.Drawing.Size(90, 34);
+            this.ButtonRandomize.TabIndex = 0;
+            this.ButtonRandomize.Text = "Randomize";
+            this.ButtonRandomize.UseVisualStyleBackColor = true;
+            this.ButtonRandomize.Click += new System.EventHandler(this.ButtonRandomize_Click);
+            // 
+            // ButtonInitialize
+            // 
+            this.ButtonInitialize.Location = new System.Drawing.Point(117, 12);
+            this.ButtonInitialize.Name = "ButtonInitialize";
+            this.ButtonInitialize.Size = new System.Drawing.Size(90, 34);
+            this.ButtonInitialize.TabIndex = 1;
+            this.ButtonInitialize.Text = "Initialize";
+            this.ButtonInitialize.UseVisualStyleBackColor = true;
+            this.ButtonInitialize.Click += new System.EventHandler(this.ButtonInitialize_Click);
+            // 
+            // ButtonSort
+            // 
+            this.ButtonSort.Location = new System.Drawing.Point(213, 12);
+            this.ButtonSort.Name = "ButtonSort";
+            this.ButtonSort.Size = new System.Drawing.Size(84, 34);
+            this.ButtonSort.TabIndex = 2;
+            this.ButtonSort.Text = "Sort";
+            this.ButtonSort.UseVisualStyleBackColor = true;
+            this.ButtonSort.Click += new System.EventHandler(this.ButtonSort_Click);
+            // 
+            // ListboxDisplay
+            // 
+            this.ListboxDisplay.FormattingEnabled = true;
+            this.ListboxDisplay.Location = new System.Drawing.Point(12, 64);
+            this.ListboxDisplay.Name = "ListboxDisplay";
+            this.ListboxDisplay.Size = new System.Drawing.Size(491, 368);
+            this.ListboxDisplay.TabIndex = 3;
+            // 
+            // FormingArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 450);
+            this.Controls.Add(this.ListboxDisplay);
+            this.Controls.Add(this.ButtonSort);
+            this.Controls.Add(this.ButtonInitialize);
+            this.Controls.Add(this.ButtonRandomize);
+            this.Name = "FormingArray";
+            this.Text = "2D Integer Array";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonRandomize;
+        private System.Windows.Forms.Button ButtonInitialize;
+        private System.Windows.Forms.Button ButtonSort;
+        private System.Windows.Forms.ListBox ListboxDisplay;
+    }
+}
+
---
+++ WeekTwoV1/FormingArray.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Name: Jae Kim
+// /Don't use code inside the initialize component anymore
+// Date: 08/02/2022
+
+namespace WeekTwoV1
+{
+    public partial class FormingArray : Form
+    {
+        public FormingArray()
+        {
+            InitializeComponent();
+        }
+        static int row = 10; // x
+        static int col = 10; // y
+        static int[,] myArray = new int [row,col];
+
+        #region DisplayArray
+        private void DisplayArray()
+        {
+            ListboxDisplay.Items.Clear();
+            for (int x = 0; x < row; x++)
+            // aligning x with a row
+            {
+                ListboxDisplay.Items.Add(" ");
+                // This is only for displaying the array
+                string oneLine = " ";
+                for (int y = 0; y < col; y++)
+                {
+                    oneLine = oneLine + "    " + myArray[x, y];
+                }
+                // at this point you can change the x & y to i & k to avoid confusion
+                // add up all little colunms and put them in a single string and print them out. 
+                ListboxDisplay.Items.Add(oneLine);
+            }
+
+        }
+        #endregion
+        #region Initialize
+        private void ButtonInitialize_Click(object sender, EventArgs e)
+        {
+            
+            for (int x = 0; x < row; x++)
+ 
+            // aligning x with a row
+            {
+                for (int y = 0; y < col; y++)
+                {
+                    myArray[x, y] = x + 1;
+                    // populating and then displaying the array
+                }
+            }
+            DisplayArray();
+        }
+        #endregion
+        #region Randomize
+        private void ButtonRandomize_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            for (int x = 0; x < row; x++)
+            // aligning x with a row
+            {
+                for (int y = 0; y < col; y++)
+                {
+                    myArray[x, y] = r.Next(100, 1000);
+                    //myArray[x, y] = x + 1;
+                    // populating and then displaying the array
+                }
+            }
+            DisplayArray();
+        }
+        #endregion
+        #region Sort
+        private void ButtonSort_Click(object sender, EventArgs e)
+        {
+            for (int row_x = 0; row_x < row; row_x++)
+            // row
+            {
+                for (int col_y = 0; col_y < col; col_y++)
+                // column
+                {
+                    for (int i = 0; i < row; i++)
+                    {
+                        for (int j = 0; j < col; j++)
+                        {
+                            if (myArray[i, j] > myArray[row_x, col_y])
+
+                            {
+                                int temp = myArray[row_x, col_y];
+                                // two items you're swapping
+                                myArray[row_x, col_y] = myArray[i, j];
+                                myArray[i, j] = temp;
+
+                            }
+                        }
+                    }
+
+                }
+            }
+            DisplayArray();
+        }
+        #endregion
+    }
+}
+
