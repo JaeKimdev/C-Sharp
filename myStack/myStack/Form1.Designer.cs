103c343d9093a99f12856cee826636be63490565
Jae Hyung Kim <smfc.mene@gmail.com>
2022-02-22 09:54:00 +0800

    TIL-2022-02-22
    Data Structure - Stack

---
+++ myStack/myStack/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace myStack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxOutput = new System.Windows.Forms.ListBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonPush = new System.Windows.Forms.Button();
+            this.buttonPop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxOutput
+            // 
+            this.listBoxOutput.FormattingEnabled = true;
+            this.listBoxOutput.Location = new System.Drawing.Point(12, 25);
+            this.listBoxOutput.Name = "listBoxOutput";
+            this.listBoxOutput.Size = new System.Drawing.Size(139, 303);
+            this.listBoxOutput.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(157, 25);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // buttonPush
+            // 
+            this.buttonPush.Location = new System.Drawing.Point(157, 51);
+            this.buttonPush.Name = "buttonPush";
+            this.buttonPush.Size = new System.Drawing.Size(75, 23);
+            this.buttonPush.TabIndex = 2;
+            this.buttonPush.Text = "Push";
+            this.buttonPush.UseVisualStyleBackColor = true;
+            this.buttonPush.Click += new System.EventHandler(this.buttonPush_Click);
+            // 
+            // buttonPop
+            // 
+            this.buttonPop.Location = new System.Drawing.Point(157, 80);
+            this.buttonPop.Name = "buttonPop";
+            this.buttonPop.Size = new System.Drawing.Size(75, 23);
+            this.buttonPop.TabIndex = 3;
+            this.buttonPop.Text = "Pop";
+            this.buttonPop.UseVisualStyleBackColor = true;
+            this.buttonPop.Click += new System.EventHandler(this.buttonPop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 349);
+            this.Controls.Add(this.buttonPop);
+            this.Controls.Add(this.buttonPush);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.listBoxOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxOutput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonPush;
+        private System.Windows.Forms.Button buttonPop;
+    }
+}
+
---
+++ myStack/myStack/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myStack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stack myStack = new Stack();
+
+        private void DisplayStack()
+        {
+            listBoxOutput.Items.Clear();
+            foreach(object obj in myStack)
+            {
+                listBoxOutput.Items.Add(obj.ToString());
+            }
+        }
+
+        private void buttonPush_Click(object sender, EventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(textBoxInput.Text)))
+            {
+                myStack.Push(textBoxInput.Text);
+                DisplayStack();
+                textBoxInput.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter Data into TextBox", "ERROR MESSAGE",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonPop_Click(object sender, EventArgs e)
+        {
+            if (myStack.Count > 0)
+            {
+                myStack.Pop();
+                DisplayStack();
+            }
+            else
+            {
+                MessageBox.Show("The queue is empty", "Information",
+                 MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
