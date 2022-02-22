9ef8cd820d95fb0ba4d43207c9d39e545ac131fe
Jae Hyung Kim <smfc.mene@gmail.com>
2022-02-22 09:53:10 +0800

    TIL-2022-02-22
    Data Structure - Queue

---
+++ Week04/Week04/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Week04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEnqueue = new System.Windows.Forms.Button();
+            this.buttonDequeue = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonPeek = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonEnqueue
+            // 
+            this.buttonEnqueue.Location = new System.Drawing.Point(179, 12);
+            this.buttonEnqueue.Name = "buttonEnqueue";
+            this.buttonEnqueue.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnqueue.TabIndex = 0;
+            this.buttonEnqueue.Text = "Enqueue";
+            this.buttonEnqueue.UseVisualStyleBackColor = true;
+            this.buttonEnqueue.Click += new System.EventHandler(this.buttonEnqueue_Click);
+            // 
+            // buttonDequeue
+            // 
+            this.buttonDequeue.Location = new System.Drawing.Point(179, 41);
+            this.buttonDequeue.Name = "buttonDequeue";
+            this.buttonDequeue.Size = new System.Drawing.Size(75, 23);
+            this.buttonDequeue.TabIndex = 1;
+            this.buttonDequeue.Text = "Dequeue";
+            this.buttonDequeue.UseVisualStyleBackColor = true;
+            this.buttonDequeue.Click += new System.EventHandler(this.buttonDequeue_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(260, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 368);
+            this.listBox1.TabIndex = 3;
+            // 
+            // buttonPeek
+            // 
+            this.buttonPeek.Location = new System.Drawing.Point(179, 70);
+            this.buttonPeek.Name = "buttonPeek";
+            this.buttonPeek.Size = new System.Drawing.Size(75, 23);
+            this.buttonPeek.TabIndex = 4;
+            this.buttonPeek.Text = "Peek";
+            this.buttonPeek.UseVisualStyleBackColor = true;
+            this.buttonPeek.Click += new System.EventHandler(this.buttonPeek_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 450);
+            this.Controls.Add(this.buttonPeek);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonDequeue);
+            this.Controls.Add(this.buttonEnqueue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEnqueue;
+        private System.Windows.Forms.Button buttonDequeue;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonPeek;
+    }
+}
+
---
+++ Week04/Week04/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Queue myTasks = new Queue();
+
+        private void DisplayQueue()
+        {
+            listBox1.Items.Clear();
+            foreach (object obj in myTasks)
+            {
+                listBox1.Items.Add(obj.ToString());
+            }
+        }
+
+        private void buttonEnqueue_Click(object sender, EventArgs e)
+        {
+            if(!(string.IsNullOrEmpty(textBox1.Text)))
+                {
+                myTasks.Enqueue(textBox1.Text);
+                textBox1.Clear();
+                DisplayQueue();
+            }
+            else
+            {
+                MessageBox.Show("Please enter Data into TextBox", "ERROR MESSAGE",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonDequeue_Click(object sender, EventArgs e)
+        {
+            if(myTasks.Count > 0)
+            {
+                myTasks.Dequeue();
+                DisplayQueue();
+            }
+            else
+            {
+                MessageBox.Show("The queue is empty", "Information",
+                  MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void buttonPeek_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = myTasks.Peek().ToString();
+        }
+    }
+}
