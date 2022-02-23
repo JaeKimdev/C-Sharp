87e80f1458476251b27d40400ce2294651e40b4e
Jae Hyung Kim <84453688+mene79@users.noreply.github.com>
2022-02-23 12:08:50 +0800

    Add Delete fuction

--- Wiki/Wiki/Wiki.Designer.cs
+++ Wiki/Wiki/Wiki.Designer.cs
@@ -38,48 +38,60 @@
             this.textBoxStructure = new System.Windows.Forms.TextBox();
             this.textBoxDefinition = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.listBoxOutput = new System.Windows.Forms.ListBox();
+            this.listViewArray = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
             this.SuspendLayout();
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(12, 15);
+            this.buttonAdd.Location = new System.Drawing.Point(12, 12);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(75, 23);
             this.buttonAdd.TabIndex = 0;
             this.buttonAdd.Text = "ADD";
             this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // buttonDelete
             // 
-            this.buttonDelete.Location = new System.Drawing.Point(93, 15);
+            this.buttonDelete.Location = new System.Drawing.Point(93, 12);
             this.buttonDelete.Name = "buttonDelete";
             this.buttonDelete.Size = new System.Drawing.Size(75, 23);
             this.buttonDelete.TabIndex = 1;
             this.buttonDelete.Text = "DELETE";
             this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
             // 
             // buttonSearch
             // 
-            this.buttonSearch.Location = new System.Drawing.Point(326, 13);
+            this.buttonSearch.Location = new System.Drawing.Point(298, 18);
             this.buttonSearch.Name = "buttonSearch";
             this.buttonSearch.Size = new System.Drawing.Size(75, 23);
             this.buttonSearch.TabIndex = 2;
             this.buttonSearch.Text = "Search";
             this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
             // buttonOpen
             // 
-            this.buttonOpen.Location = new System.Drawing.Point(233, 273);
+            this.buttonOpen.Location = new System.Drawing.Point(206, 290);
             this.buttonOpen.Name = "buttonOpen";
             this.buttonOpen.Size = new System.Drawing.Size(75, 23);
             this.buttonOpen.TabIndex = 3;
             this.buttonOpen.Text = "Open";
             this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(314, 273);
+            this.buttonSave.Location = new System.Drawing.Point(287, 290);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
             this.buttonSave.TabIndex = 4;
@@ -89,62 +101,132 @@
             // textBoxName
             // 
             this.textBoxName.AccessibleName = "";
-            this.textBoxName.Location = new System.Drawing.Point(12, 55);
+            this.textBoxName.Location = new System.Drawing.Point(12, 71);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.ReadOnly = true;
             this.textBoxName.Size = new System.Drawing.Size(156, 20);
             this.textBoxName.TabIndex = 5;
-            this.textBoxName.Text = "Name";
             // 
             // textBoxCategory
             // 
-            this.textBoxCategory.Location = new System.Drawing.Point(12, 91);
+            this.textBoxCategory.Location = new System.Drawing.Point(12, 119);
             this.textBoxCategory.Name = "textBoxCategory";
-            this.textBoxCategory.ReadOnly = true;
             this.textBoxCategory.Size = new System.Drawing.Size(156, 20);
             this.textBoxCategory.TabIndex = 6;
-            this.textBoxCategory.Text = "Category";
             // 
             // textBoxStructure
             // 
-            this.textBoxStructure.Location = new System.Drawing.Point(12, 126);
+            this.textBoxStructure.Location = new System.Drawing.Point(12, 169);
             this.textBoxStructure.Name = "textBoxStructure";
-            this.textBoxStructure.ReadOnly = true;
             this.textBoxStructure.Size = new System.Drawing.Size(156, 20);
             this.textBoxStructure.TabIndex = 7;
-            this.textBoxStructure.Text = "Structure";
             // 
             // textBoxDefinition
             // 
-            this.textBoxDefinition.Location = new System.Drawing.Point(12, 168);
+            this.textBoxDefinition.Location = new System.Drawing.Point(12, 217);
             this.textBoxDefinition.Multiline = true;
             this.textBoxDefinition.Name = "textBoxDefinition";
             this.textBoxDefinition.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBoxDefinition.Size = new System.Drawing.Size(156, 125);
             this.textBoxDefinition.TabIndex = 8;
-            this.textBoxDefinition.Text = "Definition";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(220, 15);
+            this.textBox5.Location = new System.Drawing.Point(192, 18);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 9;
             // 
-            // listBoxOutput
+            // listViewArray
             // 
-            this.listBoxOutput.FormattingEnabled = true;
-            this.listBoxOutput.Location = new System.Drawing.Point(220, 55);
-            this.listBoxOutput.Name = "listBoxOutput";
-            this.listBoxOutput.Size = new System.Drawing.Size(181, 212);
-            this.listBoxOutput.TabIndex = 10;
+            this.listViewArray.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listViewArray.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewArray.HideSelection = false;
+            this.listViewArray.Location = new System.Drawing.Point(192, 55);
+            this.listViewArray.Name = "listViewArray";
+            this.listViewArray.Size = new System.Drawing.Size(181, 219);
+            this.listViewArray.TabIndex = 2;
+            this.listViewArray.UseCompatibleStateImageBehavior = false;
+            this.listViewArray.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Category";
+            this.columnHeader2.Width = 120;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Structure";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Definition";
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(93, 41);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 14;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip.Location = new System.Drawing.Point(0, 349);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(394, 25);
+            this.toolStrip.TabIndex = 15;
+            this.toolStrip.Text = "toolStrip1";
             // 
             // Wiki
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 319);
-            this.Controls.Add(this.listBoxOutput);
+            this.ClientSize = new System.Drawing.Size(394, 374);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewArray);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBoxDefinition);
             this.Controls.Add(this.textBoxStructure);
@@ -157,6 +239,7 @@
             this.Controls.Add(this.buttonAdd);
             this.Name = "Wiki";
             this.Text = "Wiki";
+            this.Load += new System.EventHandler(this.Wiki_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,7 +257,15 @@
         private System.Windows.Forms.TextBox textBoxStructure;
         private System.Windows.Forms.TextBox textBoxDefinition;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.ListBox listBoxOutput;
+        private System.Windows.Forms.ListView listViewArray;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.ToolStrip toolStrip;
     }
 }
 
--- Wiki/Wiki/Wiki.cs
+++ Wiki/Wiki/Wiki.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +18,88 @@ namespace Wiki
         {
             InitializeComponent();
         }
+        int rowNum = 0;
+        static int rowSize = 8;
+        static int colSize = 4;
+        string[,] myArray = new string[rowSize, colSize];
+        string fileName = "definitions.dat";
+
+        #region DisplayArray
+        private void DisplayArray()
+        {
+            listViewArray.Items.Clear();
+
+            for (int x = 0; x < rowSize; x++)
+            {
+                ListViewItem lvi = new ListViewItem(myArray[x, 0]);
+                lvi.SubItems.Add(myArray[x, 1]);
+                lvi.SubItems.Add(myArray[x, 2]);
+                listViewArray.Items.Add(lvi);
+            }
+        }
+        #endregion
+
+        private void ClearTextBox()
+        {
+            textBoxName.Clear();
+            textBoxCategory.Clear();
+            textBoxStructure.Clear();
+            textBoxDefinition.Clear();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            myArray[rowNum, 0] = textBoxName.Text;
+            myArray[rowNum, 1] = textBoxCategory.Text;
+            myArray[rowNum, 2] = textBoxStructure.Text;
+            myArray[rowNum, 3] = textBoxDefinition.Text;
+            rowNum++;
+            ClearTextBox();
+            DisplayArray();
+            toolStrip.Text = "Data Added";
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            myArray[listViewArray.SelectedItems, 0] = textBoxName.Text;
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult delName = MessageBox.Show("Do you wish to delete this Name?",
+                 "Delete Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (delName == DialogResult.Yes)
+            {
+                // add code to delete record
+            }
+        }
+
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            String target = textBoxName.Text;
+            int row = 0;
+            int col = colSize - 1;
+            while ((row <= rowSize - 1) && (col >= 0))
+            {
+              
+            }
+            listViewArray.Items.Add("Not Found");
+        }
+
+        private void Wiki_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
+
+
 }
